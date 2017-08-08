4800e8cd0051ca7740013720040f4c1e09514b19
Ratson <github@ratson.name>
2017-08-08 23:58:06 +0800

    Export upsert function

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const assert = require('assert')
+
+const _ = require('lodash')
+
+module.exports = ({ db, table, object, key, updateIgnore = [] }) => {
+  const keys = _.isString(key) ? [key] : key
+  keys.forEach(field =>
+    assert(
+      _.has(object, field),
+      `Key "${field}" is missing in ${JSON.stringify(object)}`,
+    ),
+  )
+
+  const updateFields = _.difference(_.keys(_.omit(object, keys)), updateIgnore)
+  const insert = db.table(table).insert(object)
+  if (updateFields.length === 0) {
+    return db
+      .raw(`? ON CONFLICT (${_.join(keys)}) DO NOTHING RETURNING *`, [insert])
+      .then(result => _.get(result, ['rows', 0]))
+  }
+  const update = db.queryBuilder().update(_.pick(object, updateFields))
+  return db
+    .raw(`? ON CONFLICT (${_.join(keys)}) DO ? RETURNING *`, [insert, update])
+    .then(result => _.get(result, ['rows', 0]))
+}
